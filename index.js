761b5621ce6c4450fd4caaa011227f3a0fa75832
Al Cerdan Lico <alcerdan@yahoo.com>
2024-10-02 10:45:55 -0700

    completed algorithm-hastargetsum

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // look through each number in the array
+  for (let i=0; i < array.length; i++) {
+    // look through the rest of the numbers in the array
+    for (let j = i + 1; j < array.length; j++) {
+      //add the 1st array as i and the 2nd array as j then check if it the same as the target
+      if ((array[i] + array[j]) === target) {
+        return true;
+      } 
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
-*/
+*/ //O(n^2)
 
 /* 
   Add your pseudocode here
 */
+//function hasTargetSum(array, target); {}
+  //for (let i = 0; i < array.length; i++) {
+    //for (let j = i + 1; j < array.length; j++) {
+    //} if ((array[i] + array[j]) === target) {
+    //  } return true;   
+    //}
+  //}
 
 /*
   Add written explanation of your solution here
 */
+//Function has two parameters. The 1st parameter is an array of integers, the 2nd parameter is an integer.
+//Looping through the array, if the value of two of the integers in the array would equal to the 2nd parameter, it should return true.
+//If none, it should return false.
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
